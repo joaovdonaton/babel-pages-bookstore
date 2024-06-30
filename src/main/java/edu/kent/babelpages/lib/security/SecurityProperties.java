85da326cc93f5de9f877b80ac655e86b4274abf3
Joao Donaton <joao.vitor.donaton@gmail.com>
2024-06-30 20:58:05 -0300

    security properties

--- src/main/java/edu/kent/babelpages/lib/security/SecurityProperties.java
+++ src/main/java/edu/kent/babelpages/lib/security/SecurityProperties.java
@@ -1,2 +1,16 @@
-package edu.kent.babelpages.lib.security;public class SecurityProperties {
+package edu.kent.babelpages.lib.security;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("/security.properties")
+@ConfigurationProperties(prefix = "security")
+@Data
+public class SecurityProperties {
+    private String jwtSecret;
+    private String jwtIssuer;
+    private String jwtExpiration;
 }
